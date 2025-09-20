67ce07cdb9f13da34e37253882591808e5b72bd8
psicopiloto <joseretamar@psicopiloto.com>
2025-09-20 10:57:56 +0200

    Update sobre-mi.js

--- pages/sobre-mi.js
+++ pages/sobre-mi.js
@@ -10,7 +10,7 @@ export default function SobreMi() {
       <main className="flex-grow pt-20 pb-16">
         <div className="container mx-auto px-4 max-w-5xl">
           <h1 className="text-4xl font-bold mb-10 text-center">
-            Sobre mí y el proyecto Psicopiloto
+            Sobre mí
           </h1>
 
           {/* Intro */}
