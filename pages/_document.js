e87084e9e8a92a701d4d5e29179e432bc5ffa0ab
psicopiloto <joseretamar@psicopiloto.com>
2025-09-21 23:49:16 +0200

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,12 +4,24 @@ export default function Document() {
   return (
     <Html lang="es">
       <Head>
-        {/* Favicon */}
-        <link rel="icon" href="/favicon.ico" />
-        {/* Puedes añadir también versiones en PNG */}
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        {/* Favicon principal */}
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="64x64" href="/favicon-64x64.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+
+        {/* Apple Touch Icon */}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+
+        {/* Web manifest para PWA (opcional pero recomendado) */}
+        <link rel="manifest" href="/site.webmanifest" />
+
+        {/* Theme color para navegadores móviles */}
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <body>
         <Main />
