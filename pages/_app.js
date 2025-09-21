8ee64dd5048057b3caa9aafc32987563af32fb56
psicopiloto <joseretamar@psicopiloto.com>
2025-09-21 22:50:03 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,18 @@
 import "../styles/globals.css";
 import CookiesBanner from "../components/CookiesBanner";
+import { DefaultSeo } from "next-seo";
+import SEO from "../next-seo.config"; // 👈 este es tu archivo de config global
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      {/* Configuración SEO global */}
+      <DefaultSeo {...SEO} />
+
+      {/* Render de la página actual */}
       <Component {...pageProps} />
+
+      {/* Banner de cookies */}
       <CookiesBanner />
     </>
   );
