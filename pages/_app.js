79e271d71ae23ad1fe1400ba8486cb213492964e
psicopiloto <joseretamar@psicopiloto.com>
2025-09-26 10:05:34 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,9 @@
 import "../styles/globals.css";
 import CookiesBanner from "../components/CookiesBanner";
 import { DefaultSeo } from "next-seo";
-import SEO from "../next-seo.config"; 
-import BackgroundLogo from "../components/BackgroundLogo"; 
-import WhatsAppButton from "../components/WhatsAppButton"; // 👈 nuevo import
+import SEO from "../next-seo.config";
+import BackgroundLogo from "../components/BackgroundLogo";
+import WhatsAppButton from "../components/WhatsAppButton"; // 👈 Botón flotante
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -11,7 +11,33 @@ function MyApp({ Component, pageProps }) {
       {/* Configuración SEO global */}
       <DefaultSeo {...SEO} />
 
-      {/* Contenedor principal con el background */}
+      {/* JSON-LD para que Google entienda la organización y el logo */}
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "Organization",
+            name: "Psicopiloto",
+            url: "https://www.psicopiloto.com",
+            logo: "https://www.psicopiloto.com/logo.png", // ✅ Logo cuadrado (512x512 ideal)
+            contactPoint: {
+              "@type": "ContactPoint",
+              telephone: "+34 676 230 537",
+              contactType: "customer service",
+              areaServed: "ES",
+              availableLanguage: ["Spanish", "English"],
+            },
+            sameAs: [
+              "https://www.facebook.com/psicopiloto",
+              "https://www.instagram.com/psicopiloto",
+              "https://www.linkedin.com/company/psicopiloto",
+            ],
+          }),
+        }}
+      />
+
+      {/* Contenedor principal con background y elementos globales */}
       <div className="relative min-h-screen">
         <BackgroundLogo /> {/* 👈 Logo fijo en el fondo */}
         <div className="relative z-10">
