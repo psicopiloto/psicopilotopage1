fa03a8ff839ccde1b6742e52a38c32285402b777
psicopiloto <joseretamar@psicopiloto.com>
2025-09-26 10:47:41 +0200

    Create next-sitemap.config.js

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.psicopiloto.com', // Tu dominio
+  generateRobotsTxt: true,                // Genera automáticamente robots.txt
+  sitemapSize: 5000,                       // Número máximo de URLs por sitemap
+  changefreq: 'weekly',                    // Frecuencia recomendada de cambios
+  priority: 0.7,                           // Prioridad general
+  exclude: ['/aviso-legal', '/politica-cookies'], // Páginas que no quieres indexar
+};
