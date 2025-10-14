42f0d8ce7f24e4eeaa419fbb15faf43501ed3958
psicopiloto <joseretamar@psicopiloto.com>
2025-10-15 00:00:27 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+// pages/_app.js
+
 import "../styles/globals.css";
 import Head from "next/head";
 import CookiesBanner from "../components/CookiesBanner";
@@ -5,28 +7,29 @@ import { DefaultSeo } from "next-seo";
 import SEO from "../next-seo.config";
 import BackgroundLogo from "../components/BackgroundLogo";
 import WhatsAppButton from "../components/WhatsAppButton";
+// ✨ MEJORA 1: Importar la fuente 'Nunito' con next/font
+import { Nunito } from "next/font/google";
+
+// ✨ MEJORA 2: Configurar la fuente para usarla en toda la web
+const nunito = Nunito({
+  subsets: ["latin"],
+  display: "swap", // Clave para el rendimiento: evita que la fuente bloquee la carga
+  variable: "--font-nunito", // La usaremos como variable CSS en Tailwind
+});
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
-      {/* Head global con favicon */}
+    // ✨ MEJORA 3: Aplicar la variable de la fuente al contenedor principal
+    <div className={`${nunito.variable} relative min-h-screen font-sans`}>
+      {/* Head global simplificado (los favicons los gestiona next-seo) */}
       <Head>
-        {/* Favicon principal */}
-        <link rel="icon" href="/favicon.ico" />
-
-        {/* Versiones alternativas para dispositivos modernos */}
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-
-        {/* Color de tema para navegadores que lo soportan */}
         <meta name="theme-color" content="#ffffff" />
       </Head>
 
       {/* Configuración SEO global */}
       <DefaultSeo {...SEO} />
 
-      {/* JSON-LD para que Google entienda la organización y el logo */}
+      {/* JSON-LD con la ruta del logo corregida */}
       <script
         type="application/ld+json"
         dangerouslySetInnerHTML={{
@@ -35,7 +38,8 @@ function MyApp({ Component, pageProps }) {
             "@type": "Organization",
             name: "Psicopiloto",
             url: "https://www.psicopiloto.com",
-            logo: "https://www.psicopiloto.com/logo.png",
+            // ✨ MEJORA 4: Usar el logo optimizado .webp
+            logo: "https://www.psicopiloto.com/logo.webp",
             contactPoint: {
               "@type": "ContactPoint",
               telephone: "+34 676 230 537",
@@ -44,24 +48,21 @@ function MyApp({ Component, pageProps }) {
               availableLanguage: ["Spanish", "English"],
             },
             sameAs: [
-              "https://www.facebook.com/psicopiloto",
               "https://www.instagram.com/psicopiloto",
-              "https://www.linkedin.com/company/psicopiloto",
+              "https://www.linkedin.com/in/jcrodriguezret",
+              "https://twitter.com/jcrodriguezret",
             ],
           }),
         }}
       />
 
-      {/* Contenedor principal con background y elementos globales */}
-      <div className="relative min-h-screen">
-        <BackgroundLogo />
-        <div className="relative z-10">
-          <Component {...pageProps} />
-          <CookiesBanner />
-          <WhatsAppButton />
-        </div>
+      {/* Contenedor principal con elementos globales */}
+      <div className="relative z-10">
+        <Component {...pageProps} />
+        <CookiesBanner />
+        <WhatsAppButton />
       </div>
-    </>
+    </div>
   );
 }
 
